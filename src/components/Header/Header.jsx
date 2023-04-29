ec4ce6335edd659b419f64afa84df321b2110544
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-29 20:08:12 +0600

    update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/prime-logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand>
+            <img
+              alt=""
+              src={logo}
+              width="60"
+              height="60"
+            />
+            <span className='ms-2 display-6'>Tech Store</span>
+                     </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="mx-auto">
                         <Nav.Link>
                             <Link to="/">Home</Link>
                         </Nav.Link>
