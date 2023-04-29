3cb4937ccabf2345df0491d33cc4be712ce9ed69
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-29 18:24:50 +0600

    component Added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h3>Copyright 2023&copy;</h3>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3>I am from Home</h3>
+        </div>
+    );
+}
+
+export default Home;
