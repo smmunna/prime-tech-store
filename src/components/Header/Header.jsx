6dfced2796125248c463214c9ebed7aeaaab8d2e
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-30 12:26:27 +0600

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-            <Navbar.Brand>
-            <img
-              alt=""
-              src={logo}
-              width="60"
-              height="60"
-            />
-            <span className='ms-2 display-6'>Tech Store</span>
-                     </Navbar.Brand>
+                <Navbar.Brand>
+                    <img
+                        alt=""
+                        src={logo}
+                        width="60"
+                        height="60"
+                    />
+                    <span className='ms-2 display-6'>Tech Store</span>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
