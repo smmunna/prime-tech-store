f9fc4a9f5327dac8e01129ebd8d2fa442f2b6dd9
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-30 09:35:21 +0600

    Update SingleCardData.jsx

--- src/components/Store/SingleCardData/SingleCardData.jsx
+++ src/components/Store/SingleCardData/SingleCardData.jsx
@@ -6,12 +6,12 @@ const SingleCardData = ({ swt }) => {
         <div class="col">
             <div class="card h-100">
                 <div className='text-center'>
-                <img src={image} class="card-img-top w-50 text-center" alt="SmMunna" />
+                    <img src={image} class="card-img-top w-50 text-center" alt="SmMunna" />
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">{name}</h5>
-                    <h5>Price: {price==0?<span className='text-success'>Free</span>:<span className='text-danger'>${price}</span>}</h5>
-                    <p class="card-text">{description.length>50?<>{description.slice(0,50)}...</>:description}</p>
+                    <h5>Price: {price == 0 ? <span className='text-success'>Free</span> : <span className='text-danger'>${price}</span>}</h5>
+                    <p class="card-text">{description.length > 50 ? <>{description.slice(0, 50)}...</> : description}</p>
                 </div>
                 <div className=' p-2'>
                     <button className="btn btn-primary">Download Now</button>
