42367bdad05ae6eae7edf7c19652943c58419ba1
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-30 10:22:17 +0600

    Trending apps

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,36 +1,70 @@
 import React from 'react';
 import './Home.css';
-import { FaAppStoreIos,FaApple,FaCcJcb,FaConfluence } from 'react-icons/fa';
+import { FaAppStoreIos, FaApple, FaCcJcb, FaConfluence } from 'react-icons/fa';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <div className='Header-bg text-white'>
-                <h3 className='display-4'>The Best Software</h3>
-                <h3>For you in One Place</h3>
-            </div>
-            <div className='mt-4 container'>
-                <h3 className='mb-3'>Trending Content</h3>
-                <div className='trendCardGrid gap-2'>
-                    <div className='trendcard'>
-                      <span><FaAppStoreIos/></span>  <span className='ms-2'>Roblox Games</span>
-                    </div>
-                    <div className='trendcard '>
-                      <span><FaApple/></span>  <span className='ms-2'>FaAmilia Games</span>
-                    </div>
-                    <div className='trendcard '>
-                      <span><FaCcJcb/></span>  <span className='ms-2'>FaCcJcb Games</span>
-                    </div>
-                    <div className='trendcard '>
-                      <span><FaConfluence/></span>  <span className='ms-2'>FaConfluence Games</span>
-                    </div>
-                    
-                </div>
-            </div>
+  const data = useLoaderData();
+  console.log(data)
+  return (
+    <div>
+      <div className='Header-bg text-white'>
+        <h3 className='display-4'>The Best Software</h3>
+        <h3>For you in One Place</h3>
+      </div>
+      <div className='mt-4 container'>
+        <h3 className='mb-3'>Trending Content</h3>
+        <div className='trendCardGrid gap-2'>
+          <div className='trendcard'>
+            <span><FaAppStoreIos /></span>  <span className='ms-2'>Roblox Games</span>
+          </div>
+          <div className='trendcard '>
+            <span><FaApple /></span>  <span className='ms-2'>FaAmilia Games</span>
+          </div>
+          <div className='trendcard '>
+            <span><FaCcJcb /></span>  <span className='ms-2'>FaCcJcb Games</span>
+          </div>
+          <div className='trendcard '>
+            <span><FaConfluence /></span>  <span className='ms-2'>FaConfluence Games</span>
+          </div>
 
-            
         </div>
-    );
+      </div>
+
+      <div className="my-4 container">
+        <h3>Trending Apps</h3>
+        <div className='row  row-cols-1 row-cols-md-4 g-4'>
+          {
+            data.length > 4 ?
+              <>
+                {
+                  data.slice(0, 4).map(apps => {
+                    const { id, name, price, image } = apps;
+                    return <div class="col h-100" key={id}>
+                      <div class="card pt-2">
+                        <div className="text-center">
+                          <img src={image} class="card-img-top w-25" alt="..." />
+                        </div>
+                        <div class="card-body">
+                          <h5 class="card-title">{name}</h5>
+                          <h5>Price: {price == 0 ? <span className='text-success'>Free</span> : <span className='text-danger'>${price}</span>}</h5>
+                        </div>
+                        <div className=' p-2'>
+                          <button className="btn btn-primary">Download Now</button>
+                        </div>
+                      </div>
+                    </div>
+                  })
+                }
+              </>
+              :
+              <></>
+          }
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -15,7 +15,8 @@ const router = createBrowserRouter([
       children:[
         {
           path:"/",
-          element:<Home/>
+          element:<Home/>,
+          loader:()=>fetch('http://localhost:5000/products/swt')
         },
         {
           path:"/store",
