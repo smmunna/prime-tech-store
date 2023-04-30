d1ced742d775af9616c7bdd1566245f90708481a
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-30 15:32:38 +0600

    Update PrivateRoute.jsx

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -3,16 +3,16 @@ import { useContext } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = ({children}) => {
-    const {user,loading} = useContext(AuthContext)
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
     const location = useLocation();
-    if(loading){
+    if (loading) {
         return <div>Loading.....</div>
     }
-    if(user){
+    if (user) {
         return children;
     }
-    return <Navigate to="/login" state={{ from: location }} replace/>
+    return <Navigate to="/login" state={{ from: location }} replace />
 }
 
 export default PrivateRoute;
